6f574fa3490fc95e91e7c4cd60e89cec4aef6b59
Luciano Renzi <luciano@lucianorenzi.com>
2020-04-21 23:23:11 -0300

    fix junit_report unit test

--- tests/report/junit_report_test.py
+++ tests/report/junit_report_test.py
@@ -1,4 +1,5 @@
 import os
+import xml.etree.ElementTree as ET
 
 from golem.report.junit_report import generate_junit_report
 
@@ -23,52 +24,104 @@ class TestGenerateJunitReport:
 
         xml = generate_junit_report(execution['exec_dir'], execution['suite_name'], execution['timestamp'])
 
-        exp = (
-            '<?xml version="1.0" encoding="UTF-8"?>\n'
-            '<testsuites errors="1" failures="2" name="{suite_name}" tests="5" time="{time}">\n'
-            '    <testsuite errors="1" failures="2" name="{suite_name}" skipped="1" tests="5" time="{time}" timestamp="{timestamp}">\n'
-            '        <testcase classname="{cerror_cname}" name="code_error_test" time="{cerror_time}">\n'
-            '            <error message="{cerror_msg}" type="code error"/>\n'
-            '        </testcase>\n'
-            '        <testcase classname="{error_cname}" name="error_test" time="{error_time}">\n'
-            '            <failure message="{error_msg}" type="error"/>\n'
-            '        </testcase>\n'
-            '        <testcase classname="{failure_cname}" name="failure_test" time="{failure_time}">\n'
-            '            <failure message="{failure_msg}" type="failure"/>\n'
-            '        </testcase>\n'
-            '        <testcase classname="{skip_cname}" name="skip_test" time="{skip_time}">\n'
-            '            <skipped message="{skip_msg}" type="skipped"/>\n'
-            '        </testcase>\n'
-            '        <testcase classname="{success_cname}" name="success_test" time="{success_time}"/>\n'
-            '    </testsuite>\n'
-            '</testsuites>\n'
-        )
-        code_error_exec = [t for t in execution['exec_data']['tests'] if t['name'] == code_error_test][0]
-        error_exec = [t for t in execution['exec_data']['tests'] if t['name'] == error_test][0]
-        failure_exec = [t for t in execution['exec_data']['tests'] if t['name'] == failure_test][0]
-        skip_exec = [t for t in execution['exec_data']['tests'] if t['name'] == skip_test][0]
-        success_exec = [t for t in execution['exec_data']['tests'] if t['name'] == success_test][0]
-        exp = exp.format(
-            suite_name=execution['suite_name'],
-            time=execution['exec_data']['net_elapsed_time'],
-            timestamp=execution['timestamp'],
-            cerror_cname='{}.{}'.format(code_error_exec['full_name'], code_error_exec['test_set']),
-            cerror_time=code_error_exec['test_elapsed_time'],
-            cerror_msg='{}',
-            error_cname='{}.{}'.format(error_exec['full_name'], error_exec['test_set']),
-            error_time=error_exec['test_elapsed_time'],
-            error_msg='{}',
-            failure_cname='{}.{}'.format(failure_exec['full_name'], failure_exec['test_set']),
-            failure_time=failure_exec['test_elapsed_time'],
-            failure_msg='{}',
-            skip_cname='{}.{}'.format(skip_exec['full_name'], skip_exec['test_set']),
-            skip_time=skip_exec['test_elapsed_time'],
-            skip_msg='{}',
-            success_cname='{}.{}'.format(success_exec['full_name'], success_exec['test_set']),
-            success_time=success_exec['test_elapsed_time']
-        )
-        exp = exp.encode()
+        tests = execution['exec_data']['tests']
+        code_error_exec = next(t for t in tests if t['name'] == code_error_test)
+        error_exec = next(t for t in tests if t['name'] == error_test)
+        failure_exec = next(t for t in tests if t['name'] == failure_test)
+        skip_exec = next(t for t in tests if t['name'] == skip_test)
+        success_exec = next(t for t in tests if t['name'] == success_test)
+        xml = ET.fromstring(xml)
+        # testsuites
+        assert xml.tag == 'testsuites'
+        assert xml.attrib == {
+            'errors': '1',
+            'failures': '2',
+            'name': 'suite_one',
+            'tests': '5',
+            'time': str(execution['exec_data']['net_elapsed_time'])
+        }
+        assert len(xml) == 1
+        # testsuites/testsuite
+        testsuite = xml[0]
+        assert testsuite.tag == 'testsuite'
+        assert testsuite.attrib == {
+            'errors': '1',
+            'failures': '2',
+            'name': 'suite_one',
+            'skipped': '1',
+            'tests': '5',
+            'time': str(execution['exec_data']['net_elapsed_time']),
+            'timestamp': execution['timestamp']
+        }
+        assert len(testsuite) == 5
+        # testsuites/testsuite/code_error_test
+        test = next(test for test in testsuite if test.attrib['name'] == code_error_test)
+        assert test.attrib == {
+            'classname': '{}.{}'.format(code_error_exec['full_name'], code_error_exec['test_set']),
+            'name': 'code_error_test',
+            'time': str(code_error_exec['test_elapsed_time'])
+        }
+        assert len(test) == 1
+        # testsuites/testsuite/code_error_test/error
+        error = test[0]
+        assert error.tag == 'error'
+        assert error.attrib == {
+            'type': 'code error',
+            'message': '{}'
+        }
+        # testsuites/testsuite/error_test
+        test = next(test for test in testsuite if test.attrib['name'] == error_test)
+        assert test.attrib == {
+            'classname': '{}.{}'.format(error_exec['full_name'], error_exec['test_set']),
+            'name': error_test,
+            'time': str(error_exec['test_elapsed_time'])
+        }
+        assert len(test) == 1
+        # testsuites/testsuite/error_test/failure
+        failure = test[0]
+        assert failure.tag == 'failure'
+        assert failure.attrib == {
+            'type': 'error',
+            'message': '{}'
+        }
+        # testsuites/testsuite/failure_test
+        test = next(test for test in testsuite if test.attrib['name'] == failure_test)
+        assert test.attrib == {
+            'classname': '{}.{}'.format(failure_exec['full_name'], failure_exec['test_set']),
+            'name': failure_test,
+            'time': str(error_exec['test_elapsed_time'])
+        }
+        assert len(test) == 1
+        # testsuites/testsuite/failure_test/failure
+        failure = test[0]
+        assert failure.tag == 'failure'
+        assert failure.attrib == {
+            'type': 'failure',
+            'message': '{}'
+        }
+        # testsuites/testsuite/skipped_test
+        test = next(test for test in testsuite if test.attrib['name'] == skip_test)
+        assert test.attrib == {
+            'classname': '{}.{}'.format(skip_exec['full_name'], skip_exec['test_set']),
+            'name': skip_test,
+            'time': str(skip_exec['test_elapsed_time'])
+        }
+        assert len(test) == 1
+        # testsuites/testsuite/skipped_test/skipped
+        skipped = test[0]
+        assert skipped.tag == 'skipped'
+        assert skipped.attrib == {
+            'type': 'skipped',
+            'message': '{}'
+        }
+        # testsuites/testsuite/success_test
+        test = next(test for test in testsuite if test.attrib['name'] == success_test)
+        assert test.attrib == {
+            'classname': '{}.{}'.format(success_exec['full_name'], success_exec['test_set']),
+            'name': success_test,
+            'time': str(success_exec['test_elapsed_time'])
+        }
+        assert len(test) == 0
 
-        assert xml == exp
         xml_path = os.path.join(execution['exec_dir'], 'report.xml')
         assert os.path.isfile(xml_path)
