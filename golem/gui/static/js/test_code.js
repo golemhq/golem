1d608f995c62d10bb2e29287bbd3a66be04a3c94
sunil rathore <sunilbilara1994@gmail.com>
2021-06-14 20:01:56 +0530

    Realtime Autocomplete suggestion in code editor
    This autocomplete suggestion in code editor will work like below
    1. when user type any action then it will suggest matched action.
    2. when user type any page then it will suggest page.element.
    3. when user type any data then it will suggest data.header.

--- golem/gui/static/js/test_code.js
+++ golem/gui/static/js/test_code.js
@@ -6,9 +6,15 @@ const TestCode = new function() {
 
     this.codeEditor = undefined;
     this.unsavedChanges = false;
-
+    this.golemActions = [];
+    this.importedPages = [];
+	this.test_data =[];
+	
     this.initialize = function(file, testCode, codeError) {
         this.file = file;
+		TestCode.getGolemActions();
+		TestCode.getAllProjectPages();
+		TestCode.getAllData();
         if(codeError != null){
             $(".error-container").show();
             $(".error-container pre").html(codeError);
@@ -21,13 +27,18 @@ const TestCode = new function() {
             styleActiveLine: true,
             matchBrackets: true,
             indentUnit: 4,
-            indentWithTabs: false,
-            extraKeys: {
-                Tab: TestCommon.Utils.convertTabToSpaces
-            }
         });
-
         codeEditor = this.codeEditor;
+		CodeMirror.commands.autocomplete = function(cm) {
+			                        
+										 CodeMirror.simpleHint(cm, CodeMirror.pythonHint);
+										 }
+	 codeEditor.on("keyup", function (cm, event) {
+if (!cm.state.completionActive &&   /*Enables keyboard navigation in autocomplete list*/
+     (event.keyCode > 64 && event.keyCode < 220) || (event.keyCode==190)){// only when a letter key is pressed
+	 codeEditor.execCommand("autocomplete")
+    }
+});
 
         if(Global.user.projectWeight < Main.PermissionWeightsEnum.standard){
             this.codeEditor.setOption('readOnly', 'nocursor')
@@ -35,7 +46,49 @@ const TestCode = new function() {
         // set unsaved changes watcher
         this.watchForUnsavedChanges();
     }
-
+    this.getGolemActions = function(){
+        xhr.get('/api/golem/actions', {
+            project: this.file.project
+        }, golemAction => {
+			golemAction.forEach(function(action) {
+            TestCode.golemActions.push(action.name)
+        })
+        })
+		
+    }
+	this.getAllProjectPages = function(){
+        xhr.get('/api/project/pages', {
+            project: this.file.project
+        }, pages => {
+			pages.forEach(function(page){
+            TestCode.getPageContents(page)
+        });
+        })
+    }
+	this.getPageContents = function(pageName){
+        xhr.get('/api/page/components', {
+            project: this.file.project,
+            page: pageName
+        }, result => {
+            if(result.error == 'page does not exist'){
+                // mark page as not existent
+                $(`input[value='${pageName}']`).addClass('not-exist');
+            }
+            else{
+				let elemets  = result.components.elements
+				elemets.forEach(function(pagefull){
+					TestCode.importedPages.push(pagefull.full_name)	
+					})
+            }
+        })
+    }
+	this.getAllData = function()
+	{
+		let dataTableHeaders = TestCommon.DataTable.getHeaders();
+        dataTableHeaders.forEach(function(header){
+            TestCode.test_data.push(`data.${header}`)
+        });
+	}
     this.save = function(callback){
         let content = this.codeEditor.getValue();
         // get data from table
